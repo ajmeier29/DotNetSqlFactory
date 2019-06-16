13ffeaead2186271e30a2be4e0ea4ff96f5ce710
Andrew Meier <ajmeier29@gmail.com>
2019-06-16 16:05:20 -0400

    created config files

--- DotNetSqlFactory.Tests/UnitTest1.cs
+++ DotNetSqlFactory.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNetSqlFactory.DataOperations;
 
 namespace DotNetSqlFactory.Tests
 {
@@ -7,8 +9,10 @@ namespace DotNetSqlFactory.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SqlClientAppConfigValidation()
         {
+            var provider = ConfigurationManager.AppSettings["provider"];
+            SqlFactory sqlFactory = new SqlFactory(provider);
         }
     }
 }
---
+++ DotNetSqlFactory/DataOperations/SqlFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace DotNetSqlFactory.DataOperations
+{
+    public class SqlFactory
+    {
+        DbConnection _dbConnection;
+        private string _provider;
+        public SqlFactory(string provider)
+        {
+            _provider = provider;
+        }
+    }
+}
