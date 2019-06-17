3d8f8119dabc413f287be64a41aeedaf6dc6af24
Andrew Meier <ajmeier29@gmail.com>
2019-06-16 22:56:11 -0400

    SqlFactory.cs build out started, successful db connection

---
+++ DotNetSqlFactory.Tests/SqlFactory_Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNetSqlFactory.DataOperations;
+using System.Data;
+
+namespace DotNetSqlFactory.Tests
+{
+    [TestClass]
+    public class SqlFactory_Tests
+    {
+        [TestMethod]
+        public void SelectQuerySqlServer_Test()
+        {
+            var connectionstring = ConfigurationManager.AppSettings["connectionString"];
+            SqlFactory sqlFactory = new SqlFactory(SqlFactory.DataProvider.SqlServer, connectionstring);
+            sqlFactory.ExecuteQuery(@"select Make
+                                        from dbo.Inventory
+                                        where CarId = 8");
+        }
+        [TestMethod]
+        public void SqlClientDbFactory_Test()
+        {
+            SqlFactory sqlFactory = new SqlFactory(SqlFactory.DataProvider.SqlServer);
+            Assert.IsTrue(sqlFactory.DatabaseFactory.GetType().Name.Equals("SqlClientFactory"));
+        }
+        [TestMethod]
+        public void OracleDbFactory_Test()
+        {
+            SqlFactory sqlFactory = new SqlFactory(SqlFactory.DataProvider.Oracle);
+            Assert.IsTrue(sqlFactory.DatabaseFactory.GetType().Name.Equals("OracleClientFactory"));
+        }
+
+        [TestMethod]
+        public void OledbDbFactory_Test()
+        {
+            SqlFactory sqlFactory = new SqlFactory(SqlFactory.DataProvider.OleDb);
+            Assert.IsTrue(sqlFactory.DatabaseFactory.GetType().Name.Equals("OleDbFactory"));
+        }
+
+        [TestMethod]
+        public void OdbcDbFactory_Test()
+        {
+            SqlFactory sqlFactory = new SqlFactory(SqlFactory.DataProvider.Odbc);
+            Assert.IsTrue(sqlFactory.DatabaseFactory.GetType().Name.Equals("OdbcFactory"));
+        }
+
+    }
+}
--- DotNetSqlFactory/DataOperations/SqlFactory.cs
+++ DotNetSqlFactory/DataOperations/SqlFactory.cs
@@ -4,18 +4,133 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
-using System.Data.SqlClient;
+
+// different providers
 using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.OracleClient;
+using System.Data.OleDb;
+using System.Data.Odbc;
 
 namespace DotNetSqlFactory.DataOperations
 {
     public class SqlFactory
     {
         DbConnection _dbConnection;
-        private string _provider;
-        public SqlFactory(string provider)
+        public DbConnection DatabaseConnection
         {
-            _provider = provider;
+            get => _dbConnection;
+            set => _dbConnection = value;
         }
+
+        DbProviderFactory _dbFactory;
+        public DbProviderFactory DatabaseFactory
+        {
+            get => _dbFactory;
+            set => _dbFactory = value;
+        }
+        private string _connectionString;
+
+        public string ConnectionString
+        {
+            get => _connectionString;
+            set => _connectionString = value;
+        }
+
+        private string _provider;
+        public enum DataProvider
+        {
+            SqlServer,
+            Oracle,
+            OleDb,
+            Odbc,
+            None
+        }
+
+        public SqlFactory(DataProvider dataProvider, string connectionString)
+        {
+            _connectionString = connectionString;
+            SetFactory(dataProvider);
+        }
+
+        public SqlFactory(DataProvider dataProvider)
+        {
+            SetFactory(dataProvider);
+        }
+
+        /// <summary>
+        /// Returns the appropriate DbProviderFactory for the DataProvider passed in. 
+        /// </summary>
+        /// <param name="dataProvider"></param>
+        /// <returns></returns>
+        public void SetFactory(DataProvider dataProvider)
+        {
+            switch (dataProvider)
+            {
+                case DataProvider.SqlServer:
+                    _dbFactory = DbProviderFactories.GetFactory("System.Data.SqlClient");
+                    break;
+                case DataProvider.Oracle:
+                    _dbFactory = DbProviderFactories.GetFactory("System.Data.OracleClient");
+                    break;
+                case DataProvider.OleDb:
+                    _dbFactory = DbProviderFactories.GetFactory("System.Data.OleDb");
+                    break;
+                case DataProvider.Odbc:
+                    _dbFactory = DbProviderFactories.GetFactory("System.Data.Odbc");
+                    break;
+                default:
+                    var providerList = Enum.GetValues(typeof(DataProvider));
+                    throw new Exception($"Invalid DataProvider. Accepted values: {providerList}");
+            }
+        }
+        /// <summary>
+        /// Creates a connection from the current DatabaseFactory
+        /// </summary>
+        /// <returns></returns>
+        private  DbConnection CreateConnection()
+        {
+            return _dbFactory.CreateConnection();
+        }
+        /// <summary>
+        /// Opens a connection with the active DatabaseConnection. If DatabaseConnection is null, it will try to create a
+        /// new one from the current DatabaseFactory
+        /// </summary>
+        public void OpenConnection()
+        {
+            if(_dbConnection == null)
+            {
+                // try to create a connection if one has not been created
+                try
+                {
+                    _dbConnection = CreateConnection();
+                }
+                catch (NullReferenceException ex)
+                {
+                    throw new Exception("DatabaseFactory is null!! Seta DatabaseFactory before trying to open a connection!");
+                }
+            }
+            _dbConnection.ConnectionString = _connectionString; 
+            _dbConnection.Open();
+        }
+
+        public void ExecuteQuery(string sqlQuery)
+        {
+            OpenConnection();
+
+            using(DbCommand command = _dbFactory.CreateCommand())
+            {
+                command.Connection = _dbConnection;
+                command.CommandText = sqlQuery;
+                using(DbDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    while (dataReader.Read())
+                    {
+                        var test = dataReader["Make"];
+                    }
+                }
+            }
+        }
+
     }
 }
